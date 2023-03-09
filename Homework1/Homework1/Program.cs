c780ae1eb0f1d281686e9f4063400ce2de4d60c2
slave0282 <122560420+slave0282@users.noreply.github.com>
2023-03-09 23:52:52 +0100

    Add project files.

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,39 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter two numbers");
+
+
+double firstNumInput = double.Parse(Console.ReadLine());
+double secondNumInput = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the operation you would like to be executed(+,-,*,/)");
+
+string operation = Console.ReadLine();
+
+double result = 0;
+
+switch (operation) 
+{
+    case "+":
+        result = firstNumInput + secondNumInput; 
+        break;
+    case "-":
+        result = firstNumInput - secondNumInput;
+        break;
+        case "*":
+        result = firstNumInput * secondNumInput;
+        break;
+        case "/":
+        result = firstNumInput / secondNumInput; 
+        break;
+        default:
+        Console.WriteLine("Invalid operation");
+        break;
+}
+Console.WriteLine("The result is " + result);
+
+
+
+
+
