7deb48a2cc2ef845ad3265c0211f63ca531f7516
slave0282 <122560420+slave0282@users.noreply.github.com>
2023-03-10 00:07:20 +0100

    initial commit

---
+++ Homework1/Homework1 part 2/AverageNumber/AverageNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter the first number: ");
+var firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+var secondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the third number: ");
+var thirdNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the fourth number: ");
+var fourthNum = Convert.ToInt32(Console.ReadLine());
+var result = (firstNum + secondNum + thirdNum + fourthNum) / 4;
+Console.WriteLine("The average number is: " + result);
