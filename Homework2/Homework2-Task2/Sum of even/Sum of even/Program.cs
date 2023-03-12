b53d38a8572cedb921aa5bc41e8ee84dc1e7c818
slave0282 <122560420+slave0282@users.noreply.github.com>
2023-03-12 21:16:05 +0100

    .

---
+++ Homework2/Homework2-Task2/Sum of even/Sum of even/Program.cs
@@ -0,0 +1,27 @@
+﻿//Make a console application called SumOfEven.
+//Inside it create an array of 6 integers.
+//Get numbers from the input, find and print the sum of the even numbers from the array:
+
+int[] arrayOfInts = new int[6];
+Console.WriteLine("Enter six numbers: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+int secondNum = Convert.ToInt32(Console.ReadLine());
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+int fourthNum = Convert.ToInt32(Console.ReadLine());
+int fifthNum = Convert.ToInt32(Console.ReadLine());
+int sixthNum = Convert.ToInt32(Console.ReadLine());
+arrayOfInts[0] = firstNum;
+arrayOfInts[1] = secondNum;
+arrayOfInts[2] = thirdNum;
+arrayOfInts[3] = fourthNum;
+arrayOfInts[4] = fifthNum;
+arrayOfInts[5] = sixthNum;
+int sumOfEven = 0;
+for (int i = 0; i < arrayOfInts.Length; i++)
+{
+    if (arrayOfInts[i] % 2 == 0)
+    {
+        sumOfEven += arrayOfInts[i];
+    }
+}
+Console.WriteLine("The sum of the even numbers given in the array is: " + sumOfEven);
